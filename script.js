9345b9e9c7ca91c50c11daf255dcc51a57be5bc8
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-02-13 00:06:08 +0530

    Add some basic structure to the script

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
 const gameboard = (() => {
-  const gameboard = [];
+  const board = [];
+
+  for (let i = 0; i < 3; i++) {
+    board[i] = [];
+    for (let j = 0; j < 3; j++) {
+      board[i].push(['']);
+    }
+  }
+
+  showBoard();
+
+  function setMarker(row, column, marker) {
+    if (board[row][column] !== '') return;
+    board[row][column] = marker;
+    showBoard();
+  }
+
+  function getBoard() { return board; }
+
+  function clearBoard() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        array[i][j] = '';
+      }
+    }
+
+    showBoard();
+  }
+
+  function showBoard() {
+    let text = '';
+    for (let i = 0; i < 3; i++) {
+      if (i > 0) text += '\n';
+      for (let j = 0; j < 3; j++) {
+        const mark = board[i][j] !== ''
+          ? ' ' : board[i][j];
+        text += `[${mark}]`;
+      }
+    }
+
+    console.log(text);
+  }
+
+  return { setMarker, getBoard, clearBoard };
 })();
 
-const Player = () => {
+const Player = (name, marker) => {
+  function getName() {
+    return name;
+  }
 
+  function getMarker() {
+    return marker;
+  }
+
+  function setName(newName) {
+    name = newName;
+  }
+
+  function setMarker(newMarker) {
+    marker = newMarker;
+  }
+
+  return { getName, getMarker };
 };
 
 const game = (() => {

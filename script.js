058004ce2e671777c1e916f8599f3751ef13784a
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-02-13 17:09:00 +0530

    Create a working game in console

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
+const Cell = (valueID) => {
+  let value;
+  setValue(valueID);
+
+  function getValue() { return value };
+
+  function setValue(number) {
+    switch (number) {
+      case 0:
+        value = ' ';
+        break
+      case 1:
+        value = 'X';
+        break
+      case 2:
+        value = 'O';
+    }
+  };
+
+  return { setValue, getValue };
+}
+
 const gameboard = (() => {
   const board = [];
 
   for (let i = 0; i < 3; i++) {
     board[i] = [];
     for (let j = 0; j < 3; j++) {
-      board[i].push(['']);
+      board[i].push(Cell(0));
     }
   }
 
-  showBoard();
+  const rowsOfThree = (() => {
+    let array = [];
+    for (let i = 0; i < 8; i++) {
+      array.push([]);
+    }
 
-  function setMarker(row, column, marker) {
-    if (board[row][column] !== '') return;
-    board[row][column] = marker;
-    showBoard();
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (i <= 2) array[i].push(board[i][j]);
+        if (i >= 3 && i <= 5) array[i].push(board[j][i - 3]);
+        if (i === 6) array[i].push(board[j][j]);
+        if (i === 7) array[i].push(board[2 - j][j]);
+      }
+    }
+
+    return array;
+  })();
+
+  function setMarker(row, column, markerID) {
+    if (board[row][column].getValue() !== ' ') return false;
+
+    board[row][column].setValue(markerID);
+    return true;
   }
 
   function getBoard() { return board; }
@@ -21,28 +60,27 @@ const gameboard = (() => {
   function clearBoard() {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        array[i][j] = '';
+        board[i][j].setValue(0);
       }
     }
 
-    showBoard();
   }
 
-  function showBoard() {
+  function printBoard() {
     let text = '';
     for (let i = 0; i < 3; i++) {
       if (i > 0) text += '\n';
+
       for (let j = 0; j < 3; j++) {
-        const mark = board[i][j] !== ''
-          ? ' ' : board[i][j];
-        text += `[${mark}]`;
+        text += `[${board[i][j].getValue()}]`;
+
       }
     }
 
     console.log(text);
   }
 
-  return { setMarker, getBoard, clearBoard };
+  return { setMarker, getBoard, clearBoard, printBoard, rowsOfThree };
 })();
 
 const Player = (name, marker) => {
@@ -54,21 +92,91 @@ const Player = (name, marker) => {
     return marker;
   }
 
-  function setName(newName) {
-    name = newName;
-  }
-
-  function setMarker(newMarker) {
-    marker = newMarker;
-  }
-
   return { getName, getMarker };
 };
 
-const game = (() => {
+const GameController = (playerOne, playerTwo) => {
+  const players = [
+    Player(playerOne = 'One', 1),
+    Player(playerTwo = 'Two', 2),
+  ];
+  let activePlayer = players[0];
+  let cellsFilled = 0;
+  let gameOverState = false;
 
-})();
+  printNewRound();
 
-const displayController = (() => {
+  function switchActivePlayer() {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
 
-})();
+  function printNewRound() {
+    gameboard.printBoard();
+    console.log(`It's ${activePlayer.getName()}'s turn.`);
+  }
+
+  function playRound(row, column) {
+    if (gameOverState) {
+      console.log('Enter `game.restart()` if you want to play again.');
+      return;
+    }
+    const isOccupied = !gameboard
+      .setMarker(row, column, activePlayer.getMarker());
+
+    if (isOccupied) {
+      console.log('That cell is already occupied.')
+      return;
+    }
+
+    cellsFilled++;
+    checkOutcome();
+    if (gameOverState) return;
+    switchActivePlayer();
+    printNewRound();
+  }
+
+  function checkOutcome() {
+    if (cellsFilled < 5) return;
+    if (cellsFilled > 8) return gameOver(0); // Draw or Tie function maybe
+    const winningThrees = gameboard.rowsOfThree
+      .map(row => row.map(cell => cell.getValue()));
+    const hasPlayerWon = winningThrees.some(
+      row => row.every(cell => cell !== ' ' && cell === row[0])
+    );
+
+    if (!hasPlayerWon) return;
+    gameOver(1);
+  }
+
+  function gameOver(endCause) {
+    let message = '';
+
+    switch (endCause) {
+      case 0:
+        message = 'It was a draw!';
+        break;
+      case 1:
+        message = `Player ${activePlayer.getName()} is the winner!`
+    }
+
+    gameOverState = true;
+    gameboard.printBoard();
+    console.log(message);
+  }
+
+  function restart() {
+    cellsFilled = 0;
+    gameOverState = false;
+    activePlayer = players[0];
+
+    gameboard.clearBoard();
+    printNewRound();
+  }
+
+  return { playRound, restart };
+};
+
+const game = GameController();
+/* const displayController = (() => {
+
+})(); */
